3016d5541ee1ce8f53d3b6e075a592824313334c
Mark Gibbs <Mark Gibbs>
2016-04-04 00:40:50 -0700

    Initial commit of placeholder FabricDB app.

---
+++ cbat/FabricDB/app/src/main/java/connectedbyathread/com/fabricdb/FabricDbMainFragment.java
@@ -0,0 +1,22 @@
+package connectedbyathread.com.fabricdb;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FabricDbMainFragment extends Fragment {
+
+    public FabricDbMainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fabric_db_main, container, false);
+    }
+}
